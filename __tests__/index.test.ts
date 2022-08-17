ae77c1dac89cc0d69cbc8c5467ae95025384ff05
titusdmoore <titusdmoore@gmail.com>
2022-08-17 19:22:18 -0400

    Trying to get tests to run with -o

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -5,6 +5,9 @@ describe("Beginning Tests", () => {
     it("Subtract 3 from 2 equals -1", () => {
         expect(2 - 3).toBe(-1);
     });
+    it("String should not be empty", () => {
+        expect("").toBeFalsy();
+    })
 });
 
 // Isolated Modules

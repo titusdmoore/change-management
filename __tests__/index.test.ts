653d11c3abfa6843c707d12b8960a87b238702b2
titusdmoore <titusdmoore@gmail.com>
2022-08-17 19:26:04 -0400

    Need to do all tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,8 +2,8 @@ describe("Beginning Tests", () => {
     it("Add 2 and 3 equals 5", () => {
         expect(2 + 3).toBe(5);
     });
-    it("Subtract 3 from 2 equals -1", () => {
-        expect(2 - 3).toBe(-1);
+    it("Subtract 4 from 2 equals -2", () => {
+        expect(2 - 4).toBe(-2);
     });
     it("String should not be empty", () => {
         expect("").toBeFalsy();

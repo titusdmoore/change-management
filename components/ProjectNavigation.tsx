4734e918ec9ba43fc0ea441f25afcbeb22bdaa3c
titusdmoore <titusdmoore@gmail.com>
2022-08-23 20:51:49 -0400

    Dynamic change

--- components/ProjectNavigation.tsx
+++ components/ProjectNavigation.tsx
@@ -1,7 +1,10 @@
 import { Disclosure } from "@headlessui/react";
+import { useState } from "react";
+import { BsNutFill } from "react-icons/bs";
 
 export default function ProjectNavigation(props: any) {
   const clients = props.clients;
+  const [activeClients, setActiveClients] = useState<number[]>([]);
 
   return (
     <div className="h-full bg-white w-56 p-4">
@@ -11,22 +14,28 @@ export default function ProjectNavigation(props: any) {
         </button>
         {
           clients.map((client: any, index: any) => {
+            let open = activeClients.includes(index) ? true : false;
+            
             return (
               <Disclosure as="li" key={`client-${index}`}>
-                <Disclosure.Button className="w-full text-left p-2 hover:bg-primary-active child:hover:text-primary rounded-t-lg">
-                  <span className="text-black text-lg">{client.name}</span>
-                </Disclosure.Button>
-                <Disclosure.Panel className="drop-shadow-md px-2">
-                  <ul>
-                    {
-                      client.projects.map((project: any, index: any) => {
-                        return (
-                          <li className="text-body" key={`project-${index}`}>{project.projectName}</li>
-                        );
-                      })
-                    }
-                  </ul>
-                </Disclosure.Panel>
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="w-full text-left p-2 hover:bg-primary-active child:hover:text-primary rounded-t-lg">
+                      <span className="text-black text-lg">{client.name}</span>
+                    </Disclosure.Button>
+                    <Disclosure.Panel className="drop-shadow-md px-2">
+                      <ul>
+                        {
+                          client.projects.map((project: any, index: any) => {
+                            return (
+                              <li className="text-body" key={`project-${index}`}>{project.projectName}</li>
+                            );
+                          })
+                        }
+                      </ul>
+                    </Disclosure.Panel>
+                  </>
+                )}
               </Disclosure>
             );
           })

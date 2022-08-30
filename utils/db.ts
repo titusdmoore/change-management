d18dbc388dea2e81d218f82e91d77da974463378
Titus Moore <tmoore@edgewebware.com>
2022-08-30 16:18:00 -0400

    DB query added

--- utils/db.ts
+++ utils/db.ts
@@ -10,11 +10,15 @@ const db = mysql({
   }
 });
 
+export function getWorkspace(id: number) {
+  const query = "SELECT w.id as 'Workspace Id', w.name as 'Workspace Name', p.id as 'Project Id', p.name as 'Project Name', p.description as 'Project Description', s.name as 'Project Status' FROM workspace as w INNER JOIN project as p ON p.workspaceId = w.id INNER JOIN status as s ON s.id = p.statusId;";
+}
+
 export function parseDataResponse(data: any) {
   return JSON.parse(JSON.stringify(data));
 }
 
-export default async function excuteQuery( query: any, values?: any ) {
+export default async function excuteQuery( query: any, values?: any[] ) {
   try {
     const results = await db.query(query, values);
     await db.end();
